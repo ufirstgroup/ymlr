4babc392c267c2b70c0dd2b0684c10710aaf96cc
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-04-01 16:49:46 +0200

    upgrade version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ymlr.MixProject do
     [
       app: :ymlr,
       description: "A YAML encoder for Elixir",
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.10",
       deps: deps(),
       dialyzer: dialyzer(),
