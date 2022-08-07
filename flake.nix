0e2b73eb8bd118f326246aa6eec26556e00d7ce7
Jean-Luc Geering <jlgeering@users.noreply.github.com>
2022-08-07 08:43:11 +0200

    add build deps

--- flake.nix
+++ flake.nix
@@ -6,14 +6,38 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
+  # see https://ejpcmac.net/blog/using-nix-in-elixir-projects/
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        elixir = pkgs.elixir;
+        # elixir = pkgs.beam.packages.elixir;
+        # elixir = pkgs.beam.packages.erlangR23.elixir_1_12;
+
       in {
         devShell = pkgs.mkShell {
           packages = [
-            pkgs.elixir
+            elixir
           ];
+          buildInputs = []
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            # For ExUnit Notifier on Linux.
+            pkgs.libnotify
+
+            # For file_system on Linux.
+            pkgs.inotify-tools
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin ([
+            # For ExUnit Notifier on macOS.
+            pkgs.terminal-notifier
+
+            # For file_system on macOS.
+            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+            pkgs.darwin.apple_sdk.frameworks.CoreServices
+          ]);
+
           shellHook = ''
             echo "ELIXIR: $(elixir --short-version)"
           '';
