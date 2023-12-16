5b7bdc05ed60b43fe39caabca325a3fd2e248e1b
Michael Ruoss <michael@michaelruoss.ch>
2023-12-16 20:36:18 +0100

    remove support for Elixir 1.10 and add 1.16

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Ymlr.MixProject do
       app: :ymlr,
       description: "A YAML encoder for Elixir",
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       deps: deps(),
       dialyzer: dialyzer(),
       package: package(),
